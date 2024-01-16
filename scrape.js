cb6abdc1734907ef734a286a6205f41175fa1aa3
Zahid Hossain <zpangolin@outlook.com>
2024-01-16 10:26:15 -0600

    first commit

---
+++ scrape.js
@@ -0,0 +1,5 @@
+// https://www.apartments.com/aviana-apartments-mountain-view-ca/z4nchrz/
+
+let name = document.getElementById("propertyName").textContent.trim()
+let address = document.querySelector("#mapSection > div.propertyRow > div > div").innerText.substring("Property Address: ".length)
+
